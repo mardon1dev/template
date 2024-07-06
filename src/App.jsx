b4797c9b7487e4a0ed8103f553ebd5e0b8972546
mardon1dev <dusbekovmardonbek@gmail.com>
2024-07-06 21:11:48 +0500

    Template

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Fragment } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <Fragment>
+      <div className='bg-[#151515]'>
+        <div className='container'>
+          <div className='w-full h-[100vh] flex items-center justify-center'>
+            <h1 className='text-center font-bold text-[50px] text-[#fff] animate-bounce'>Welcome to a new Project!!!</h1>
+          </div>
+        </div>  
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
